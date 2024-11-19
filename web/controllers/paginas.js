79a32f8bd75c52cfe3d7f686897658ab6aa3c6ea
DiegoArreola2003 <diegoarreola88.aragon.unam.mx>
2024-11-19 15:34:23 -0600

    cambios

--- web/controllers/paginas.js
+++ web/controllers/paginas.js
@@ -1,47 +1,64 @@
 import Usuario from "../models/Usuario.js";
-import {check, validationResult} from 'express-validator';
-import {Op} from 'sequelize'
-
-const inicio = (req, res)=>{
-    res.render("inicio",{
+import Producto from "../models/Producto.js"; // Modelo para productos
+import { check, validationResult } from "express-validator";
+import { Op } from "sequelize";
+const inicio = (req, res) => {
+    res.render("inicio", {
         pagina: "Bienvenido",
     });
-}
+};
 
-const xbox = (req, res)=>{
-    res.render('layout/bXbox',{
-        pagina: "Inico xbox",
+const log = (req, res) => {
+    res.render("credenciales/login", {
+        pagina: "Inicio xbox",
     });
-}
+};
 
-const log = (req, res)=>{
-    res.render('credenciales/login' ,{
-        pagina: "Inico xbox",
+const sing = (req, res) => {
+    res.render("credenciales/signin", {
+        pagina: "Inicio xbox",
     });
-}
+};
 
-const sing = (req, res)=>{
-    res.render('credenciales/signin' ,{
-        pagina: "Inico xbox",
-    });
-}
+const videojuegos = async (req, res) => {
+    try {
+        // Recuperar videojuegos de la base de datos
+        const productos = await Producto.findAll({
+            where: {
+                id_plataforma: 1, // Filtrar por categoría si es necesario
+                id_categoria: 1,
+            },
+        });
+        console.log(productos)
+        res.render("layout/bVideojuegos", {
+            pagina: "Inicio videojuegos",
+            productos, // Pasar los datos a la vista
+        });
+    } catch (error) {
+        console.error("Error al recuperar videojuegos:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+};
 
-const play = (req, res)=>{
-    res.render('layout/bPlay',{
-        pagina: "Inico play station",
-    });
-}
+const con = async (req, res) => {
+    try {
+        // Recuperar consolas de la base de datos
+        const consolas = await Producto.findAll({
+            where: {
+                id_categoria: {
+                    [Op.in]: [2, 3, 4], // Lista de categorías permitidas
+                },
+            },
+        });
 
-const nin = (req, res)=>{
-    res.render('layout/bNin',{
-        pagina: "Inico nintendo",
-    });
-}
-const con = (req, res)=>{
-    res.render('layout/bConsolas', {
-        pagina: "Inico consolas",
-    });
-}
-export{
-    xbox,play,nin,con,log,sing,inicio
-}
+        res.render("layout/bConsolas", {
+            pagina: "Inicio consolas",
+            consolas, // Pasar los datos a la vista
+        });
+    } catch (error) {
+        console.error("Error al recuperar consolas:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+
+export { videojuegos, con, log, sing, inicio };
